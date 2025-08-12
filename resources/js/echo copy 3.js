8223273db2152d2a0f7a1d4267d5b012eb674c5b
Wagner Anacleto <wagner.anacleto@PMCG.IMTI>
2025-08-11 22:12:21 -0400

    reverb

--- resources/js/echo copy 3.js
+++ resources/js/echo copy 3.js
@@ -11,5 +11,4 @@ window.Echo = new Echo({
     wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
     enabledTransports: ['ws', 'wss'],
-    path: '/reverb', // <-- adicionado para usar o proxy reverso
 });
